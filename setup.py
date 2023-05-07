5683f988104c81f38e5d3b9d1caabb6d962415fb
Prajwal011 <60548576+Prajwal011@users.noreply.github.com>
2023-05-07 18:49:02 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='toogle_cell',
     version='0.0.1',
     description='My private package from private github repo',
-    url='https://github.com/Prajwal011/dbxyz.git',
+    url='https://github.com/Prajwal011/toggle_cell.git',
     author='PJ',
     author_email='prajwal.aiadventures@gmail.com',
     license='unlicense'
