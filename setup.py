b9f2314b762dbbc9253acc3fc2b25e20a327c2e5
Prajwal011 <60548576+Prajwal011@users.noreply.github.com>
2023-05-07 14:35:14 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,13 @@
-import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="dbxyz",
-    version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Prajwal011/dbxyz.git", 
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+from setuptools import setup
+setup(
+    name='toogle_cell',
+    version='0.0.1',
+    description='My private package from private github repo',
+    url='https://github.com/Prajwal011/dbxyz.git',
+    author='PJ',
+    author_email='prajwal.aiadventures@gmail.com',
+    license='unlicense',
+    packages=['toggle_cell'],
+    zip_safe=False
 )
