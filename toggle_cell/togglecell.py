f5c219492a77eebf2417ad5cbe85334856b657a8
Prajwal011 <60548576+Prajwal011@users.noreply.github.com>
2023-05-08 15:40:24 +0530

    Update togglecell.py

--- toggle_cell/togglecell.py
+++ toggle_cell/togglecell.py
@@ -115,6 +115,7 @@ class tg_c:
         def hint(x,auth='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'):
 #             if self.auth=='' or self.auth==None or len(self.auth)<2:
 #                 self.auth='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'
+            print(auth)
             openai.api_key=auth
             text=f'''{x} just give me steps or algorithm not code for this please'''
             d={'role':'user',"content":text}
