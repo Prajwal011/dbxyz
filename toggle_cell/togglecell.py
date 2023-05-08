d8d0364729bcdcdad8dab56a62c05ad01586f36e
Prajwal011 <60548576+Prajwal011@users.noreply.github.com>
2023-05-08 17:46:12 +0530

    Update togglecell.py

--- toggle_cell/togglecell.py
+++ toggle_cell/togglecell.py
@@ -75,7 +75,7 @@ class tg_c:
                 def on_button_clicked(b):
                     que = input_box.value
                     if auth.strip()!='':
-                        print(auth)
+#                         print(auth)
                         print(hint(que,auth))
 #                     else:
 #                         print(hint(''' {}'''.format(que)))
@@ -113,9 +113,9 @@ class tg_c:
         def hint(x,auth):
 #             if self.auth=='' or self.auth==None or len(self.auth)<2:
 #                 self.auth='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'
-            print(auth)
+#             print(auth)
             openai.api_key=auth.strip()
-            print(auth,x,len(x))
+#             print(auth,x,len(x))
             text=f'''{x} just give me steps or algorithm not code for this please'''
             d={'role':'user',"content":text}
             responses=[]
